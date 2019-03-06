34e8a1793ca862238db164b275aa9102bc7227b2
Swapna-Sahu <swapna.sahu85@gmail.com>
2019-03-06 10:06:52 +0100

    created method as per homework

---
+++ javascript/javascript1/week1/dog_age_calc_homework.js
@@ -0,0 +1,21 @@
+var dogYearOfBirth =2015;
+var dogYearFuture =2019;
+var dogYears;
+var humanYears;
+
+dogYears = dogYearFuture - dogYearOfBirth;
+humanYears = dogYears*7;
+
+var shouldShowResultInDogYears = true;
+
+if(shouldShowResultInDogYears === true)
+ {
+var output = "Your dog will be "+ dogYears +" dog years old in "+dogYearFuture ;
+console.log(output);
+ }
+else
+ {
+  var output = "Your dog will be "+ humanYears +" human years old in "+dogYearFuture;
+  console.log(output);
+ }
+

61c3249fca2c4418425ec1ba5451aaadf900783c
Swapna-Sahu <swapna.sahu85@gmail.com>
2019-03-06 21:01:14 +0100

    changed variable name

--- javascript/javascript1/week1/startupName.js
+++ javascript/javascript1/week1/startupName.js
@@ -1,10 +1,11 @@
 var firstWords= ["Hello","Awesome","Wonderful","Beautiful","Peaceful","Easy","Strong","New","Quick","Magical"];
-
 var secondWords= ["World","Earth","Planet","Creature","Nature","Life","Mind","Life","Solution","Universa"];
 
-var x = Math.floor(Math.random()*10)+0;
+var randomNumber = Math.floor(Math.random()*10)+0;
+
 /*--------------Name with two words ---------------------*/
-var startupName =firstWords[x]+" "+ secondWords[x]; 
-var numberOfChar = firstWords[x].length+secondWords[x].length;
+
+var startupName =firstWords[randomNumber]+" "+ secondWords[randomNumber]; 
+var numberOfChar = firstWords[randomNumber].length+secondWords[randomNumber].length;
 var output = "The startup: "+startupName+" contains "+numberOfChar+ " characters ";
 console.log(output);

ecab3eb2c693b06dcb3371ad165374732a71f67e
Swapna-Sahu <swapna.sahu85@gmail.com>
2019-03-06 20:50:42 +0100

    added Julia housing price checking

--- javascript/javascript1/week1/house_price.js
+++ javascript/javascript1/week1/house_price.js
@@ -1,26 +1,31 @@
-/*-------------Check with both name (Peter and Julie) and variables-----------*/
+/*-------------House Price Checking for Peter-----------*/
 
-var name ="Peter";
 var wide= 8;
-
 var deep=10;
-
 var height=10;
-
 var gardenSizeInM2=100;
-
 var actualPrice = 250000;
 
-/*-----------function for checking house price----------------------*/
-
 var volumeInMeters = wide * deep * height;
-
 var housePrice = volumeInMeters*2.5*1000+gardenSizeInM2*300;
-
-
-/*----------------Comparing the price differnece------------------*/
 
 output = ((housePrice < actualPrice)? "too low" : "too high");
 
-var statement = "The actual price of house is "+actualPrice+ "and "+ name + " is paying "+housePrice+ " which is "+output+".";
+var statement = "The actual price of house is "+actualPrice+ "and Peter is paying "+housePrice+ " which is "+output+".";
 console.log(statement);
+
+/*--------------------House Price Checking for Julia-------------------*/
+var wide= 5;
+var deep=11;
+var height=8;
+var gardenSizeInM2=70;
+var actualPrice = 1000000;
+
+var volumeInMeters = wide * deep * height;
+var housePrice = volumeInMeters*2.5*1000+gardenSizeInM2*300;
+
+output = ((housePrice < actualPrice)? "too low" : "too high");
+
+var statement = "The actual price of house is "+actualPrice+ "and Julia is paying "+housePrice+ " which is "+output+".";
+console.log(statement);
+

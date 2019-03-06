9fec7689108e897690004ecbbedd439c0ec336ed
Swapna-Sahu <swapna.sahu85@gmail.com>
2019-03-06 09:37:43 +0100

    Actual method of Dog age Calculation

---
+++ javascript/javascript1/week1/dog_age_calc.js
@@ -0,0 +1,33 @@
+var dogYearOfBirth =2015;
+var dogYearFuture =2019;
+var dogYears;
+var humanYears;
+
+/*----------- Actual Dog Age calculation ------------------------*/
+
+dogYears = dogYearFuture - dogYearOfBirth;
+var dogYears=4;
+
+if(dogYears==1)
+{
+humanYears=dogYears*15;
+} 
+else if(dogYears==2)
+{
+humanYears=24;
+}
+else 
+{
+humanYears=24+4*(dogYears-2);
+}
+
+/*-----------------------Working with Boolean --------------*/
+
+var shouldShowResultInDogYears = false;
+if(shouldShowResultInDogYears){
+var output = "Your dog will be "+ dogYears +" dog years old in "+dogYearFuture ;
+console.log(output);
+}else{
+var output = "Your dog will be "+ humanYears +" human years old in "+dogYearFuture;
+console.log(output);
+}

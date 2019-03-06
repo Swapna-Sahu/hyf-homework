534bea51ec17d19088fbf574151135b0caf3878b
Swapna-Sahu <swapna.sahu85@gmail.com>
2019-03-06 13:23:16 +0100

    added future age calc

--- javascript/javascript1/week1/age_calc.js
+++ javascript/javascript1/week1/age_calc.js
@@ -2,7 +2,7 @@
 var yearOfBirth = 1988;
 var futureYear = 2019;
 
-age = futureYear - yearOfBirth ;
+var age = futureYear - yearOfBirth ;
 
 var output = "You will be "+age+" years old in "+ yearFuture;
 console.log(output);
